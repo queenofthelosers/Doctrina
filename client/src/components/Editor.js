83eb8a453b1258d8b1e491a720a98082363b263e
HarshvardhanJha1 <harshvardhanjha1@gmail.com>
2021-03-28 17:47:14 +0530

    Installed package

--- client/src/components/Editor.js
+++ client/src/components/Editor.js
@@ -157,22 +157,6 @@ class Editor extends React.Component
                 
                 </div>
                 <div className = "fileUploader">
-                    {/* <div className="file has-name is-boxed">
-                    <label className="file-label">
-                        <input className="file-input" type="file" name="resume"/>
-                        <span className="file-cta">
-                        <span className="file-icon">
-                            <i className="fas fa-upload"></i>
-                        </span>
-                        <span className="file-label">
-                            Choose a file…
-                        </span>
-                        </span>
-                        <span className="file-name">
-                        
-                        </span>
-                    </label>
-                    </div> */}
                     <FileUploadComponent></FileUploadComponent>
                 </div>
             </div>
